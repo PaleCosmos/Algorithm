25e1db9e52afd21d03f2c281719431dc59c7ad62
Sanghyeon Park <tkdgusdlqhek@hanmail.net>
2019-12-10 13:11:02 +0900

    InsertionSort

--- Sorting/InsertionSort/InsertionSort.cpp
+++ Sorting/InsertionSort/InsertionSort.cpp
@@ -20,7 +20,18 @@ using namespace std;
 
 void insertionSort(int *arr)
 {
-   
+    for (int i = 1; i < size; i++)
+    {
+        int j;
+        int key = arr[i];
+
+        for (j = i - 1; j >= 0 && arr[j] > key; j--)
+        {
+            arr[j + 1] = arr[j];
+        }
+
+        arr[j + 1] = key;
+    }
 }
 
 void print(int *arr, string define)
