47010ee3b5aaf9434a6c1cf38d4028ac8d85b89d
Sanghyeon Park <tkdgusdlqhek@hanmail.net>
2019-12-19 14:36:42 +0900

    linked list (ing)

---
+++ DataStructure/Data.h
@@ -0,0 +1,45 @@
+#pragma region LICENSE
+//    Copyright 2019 PaleCosmos
+//
+//    Licensed under the Apache License, Version 2.0 (the "License");
+//    you may not use this file except in compliance with the License.
+//    You may obtain a copy of the License at
+//
+//        http://www.apache.org/licenses/LICENSE-2.0
+//
+//    Unless required by applicable law or agreed to in writing, software
+//    distributed under the License is distributed on an "AS IS" BASIS,
+//    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//    See the License for the specific language governing permissions and
+//    limitations under the License.
+#pragma endregion
+#include <iostream>
+#include <stdlib.h>
+
+using namespace std;
+
+struct Node
+{
+    string data;
+    Node *next;
+    Node *last;
+};
+
+void print(Node* head)
+{
+    if(head == NULL)
+    {
+        cout<<"The header is NULL pointer."<<endl;
+        return;
+    }
+
+    Node * temp = head;
+
+    cout<<temp->data;
+    temp = temp->next;
+    while(temp!=NULL)
+    {
+        cout<<" -> "<<temp->data;
+        temp = temp->next;
+    }
+}
