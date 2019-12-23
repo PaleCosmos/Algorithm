592d4ab782ed9944f3cb5a878886dbb81b4310f8
Sanghyeon Park <tkdgusdlqhek@hanmail.net>
2019-12-24 01:30:16 +0900

    Queue

--- DataStructure/Queue/Queue.cpp
+++ DataStructure/Queue/Queue.cpp
@@ -14,3 +14,93 @@
 //    limitations under the License.
 #pragma endregion
 #include "../Data.h"
+
+void enqueue(NodePtr *, NodePtr *, string, int *);
+void dequeue(NodePtr *, NodePtr *, int *);
+
+int main()
+{
+    Node *head = NULL;
+    Node *tail = NULL;
+    int order, size = 0;
+    string value;
+
+    cout << "Welcome. This is queue example written in C++.\n"
+         << endl;
+
+    do
+    {
+        print(head);
+        managePointer(head, tail);
+        cout << "size : " << size << endl;
+        cout << "1) Enqueue" << endl;
+        cout << "2) Dequeue" << endl;
+
+        cin >> order;
+
+        cout << "\n\n"
+             << endl;
+        switch (order)
+        {
+        case 1:
+            cout << "string value : ";
+            cin >> value;
+            enqueue(&head, &tail, value, &size);
+            break;
+        case 2:
+            dequeue(&head, &tail, &size);
+            break;
+        default:
+            order = -1;
+            break;
+        }
+        cout << "\n"
+             << endl;
+    } while (order != -1);
+
+    cout << "Process Exit" << endl;
+    return 0;
+}
+
+void enqueue(NodePtr *head, NodePtr *tail, string value, int *size)
+{
+    NodePtr temp = createNewNode(value);
+
+    if (*size == 0)
+    {
+        (*head) = temp;
+        (*tail) = temp;
+    }
+    else
+    {
+        temp->next = (*head);
+        (*head)->last = temp;
+        (*head) = temp;
+    }
+
+    (*size)++;
+}
+void dequeue(NodePtr *head, NodePtr *tail, int *size)
+{
+    NodePtr temp = (*tail);
+
+    if (*size == 0)
+    {
+        cout << "The size is 0." << endl;
+        return;
+    }
+    else if (*size == 1)
+    {
+        (*tail) = NULL;
+        (*head) = NULL;
+    }
+    else
+    {
+        (*tail) = (*tail)->last;
+        (*tail)->next = NULL;
+    }
+
+    free(temp);
+
+    (*size)--;
+}
---
+++ DataStructure/Queue/Queue.go
@@ -0,0 +1,143 @@
+/*
+   Copyright 2019 PaleCosmos
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+type Node struct {
+	data string
+	next *Node
+	last *Node
+}
+
+func main() {
+	var head *Node = nil
+	var tail *Node = nil
+
+	order := 0
+	size := 0
+	var value string
+
+	fmt.Printf("Welcome. This is queue example written in Go. \n\n")
+
+	for order != -1 {
+		print(head)
+		managePointer(head, tail)
+		fmt.Println("size : " + strconv.Itoa(size))
+		fmt.Println("1) Enqueue")
+		fmt.Println("2) Dequeue")
+
+		fmt.Scanln(&order)
+
+		switch order {
+		case 1:
+			fmt.Printf("string value : ")
+			fmt.Scanln(&value)
+			enqueue(&head, &tail, value, &size)
+			break
+		case 2:
+			dequeue(&head, &tail, &size)
+			break
+
+		default:
+			order = -1
+			break
+		}
+
+		fmt.Printf("\n\n")
+	}
+
+	fmt.Println("Process Exit")
+}
+func createNewNode(value string) *Node {
+	temp := &Node{value, nil, nil}
+	return temp
+}
+
+func print(head *Node) {
+	if head == nil {
+		fmt.Println("nil")
+		return
+	}
+
+	temp := head
+
+	fmt.Print("nil <-> ")
+
+	for temp != nil {
+		fmt.Print((*temp).data + " <-> ")
+		temp = (*temp).next
+	}
+	fmt.Println("nil")
+
+}
+
+func managePointer(head *Node, tail *Node) {
+	var headStr, tailStr string
+
+	if head == nil {
+		headStr = "nil"
+	} else {
+		headStr = (*head).data
+	}
+
+	if tail == nil {
+		tailStr = "nil"
+	} else {
+		tailStr = (*tail).data
+	}
+
+	fmt.Println("Head : " + headStr)
+	fmt.Println("Tail : " + tailStr)
+}
+
+func enqueue(head **Node, tail **Node, value string, size *int) {
+	temp := createNewNode(value)
+
+	if *size == 0 {
+		(*head) = temp
+		(*tail) = temp
+	} else {
+		(**head).last = temp
+		(*temp).next = (*head)
+		(*head) = temp
+	}
+
+	(*size)++
+}
+
+func dequeue(head **Node, tail **Node, size *int) {
+
+	if *size == 0 {
+		fmt.Println("The size is 0.")
+		return
+	} else if *size == 1 {
+		(*tail) = nil
+		(*head) = nil
+	} else {
+		(*tail) = (**tail).last
+		(**tail).next = nil
+	}
+
+	// Go language has Garbage Collector.
+	// temp = nil
+
+	(*size)--
+}
