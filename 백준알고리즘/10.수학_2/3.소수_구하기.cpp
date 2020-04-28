dc32f272e615567fd9e90abd99c401ebb97044e6
Sanghyeon Park <tkdgusdlqhek@hanmail.net>
2020-04-28 22:27:08 +0900

    1

---
+++ 백준알고리즘/10.수학_2/3.소수_구하기.cpp
@@ -0,0 +1,81 @@
+#pragma region LICENSE
+//    Copyright 2020 PaleCosmos
+//
+//    Licensed under the Apache License, Version 2.0 (the "License");
+//    you may not use this file except in compliance with the License.
+//    You may obtain a copy of the License at
+//
+//        http://www.apache.org/licenses/LICENSE-2.0
+//
+//    Unless required by applicable law or agreed to in writing, software
+//    distributed under the License is distributed on an "AS IS" BASIS,
+//    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//    See the License for the specific language governing permissions and
+//    limitations under the License.
+#pragma endregion
+#include <stdio.h>
+#include <cmath>
+
+using namespace std;
+
+bool isPrime(int n)
+{
+    if (n & 1 && n ^ 1)
+    {
+        int t = (n + 1) / 2;
+        for (int i = 2; i <= t; i += 2)
+        {
+            if (!(n % i))
+            {
+                return false;
+            }
+
+            if (!(i & 1))
+                i--;
+        }
+
+        return true;
+    }
+
+    return n == 2;
+}
+
+int main()
+{
+    int n, m, i, g;
+    bool *arr;
+
+    scanf("%d %d", &n, &m);
+
+    arr = new bool[m - n + 1];
+
+    for (i = 0; i <= m - n; i++)
+        arr[i] = false;
+
+    for (i = 1; i <= sqrt(m); i++)
+    {
+        if (i ^ 1 && isPrime(i))
+        {
+            for (g = 2 * i; g <= m; g += i)
+            {
+                if (g >= n)
+                    arr[g - n] = true;
+            }
+        }
+        else
+        {
+            if (i >= n)
+                arr[i - n] = true;
+        }
+    }
+    for (i = 0; i <= m - n; i++)
+    {
+        if (!arr[i])
+        {
+            printf("%d\n", i + n);
+        }
+    }
+    delete[] arr;
+
+    return 0;
+}
