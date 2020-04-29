1c3caec89525505446f131687df144396e1f2702
Sanghyeon Park <tkdgusdlqhek@hanmail.net>
2020-04-29 18:41:44 +0900

    ㅇ

---
+++ 백준알고리즘/10.수학_2/8.직각삼각형.cpp
@@ -0,0 +1,34 @@
+#pragma region LICENSE
+//    Copyright 2020 PaleCosmos
+//
+//    Licensed under the Apache License, Version 2.0 (the "License");
+//    you may not use this file except in compliance with the License.
+//    You may obtain a copy of the License at
+//
+//        http://www.apache.org/licenses/LICENSE-2.0
+//
+//    Unless required by applicable law or agreed to in writing, software
+//    distributed under the License is distributed on an "AS IS" BASIS,
+//    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//    See the License for the specific language governing permissions and
+//    limitations under the License.
+#pragma endregion
+#include <stdio.h>
+
+using namespace std;
+
+int main()
+{
+    int a, b, c;
+
+    for (; scanf("%d %d %d", &a, &b, &c) && a | b | c;)
+    {
+        a *= a;
+        b *= b;
+        c *= c;
+
+        printf("%s\n", a + b == c || a + c == b || b + c == a ? "right" : "wrong");
+    }
+
+    return 0;
+}
---
+++ 백준알고리즘/10.수학_2/9.택시_기하학.cpp
@@ -0,0 +1,33 @@
+#pragma region LICENSE
+//    Copyright 2020 PaleCosmos
+//
+//    Licensed under the Apache License, Version 2.0 (the "License");
+//    you may not use this file except in compliance with the License.
+//    You may obtain a copy of the License at
+//
+//        http://www.apache.org/licenses/LICENSE-2.0
+//
+//    Unless required by applicable law or agreed to in writing, software
+//    distributed under the License is distributed on an "AS IS" BASIS,
+//    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//    See the License for the specific language governing permissions and
+//    limitations under the License.
+#pragma endregion
+#include <stdio.h>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    int r;
+    double r2;
+
+    scanf("%d", &r);
+    r2 = r * r;
+
+    printf("%.6f\n", M_PI * r2);
+    printf("%.6f\n", 2 * r2);
+
+    return 0;
+}
